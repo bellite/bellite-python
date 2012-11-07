4fd6eeaced36712c0b57b0c44367f87d6610c69f
"Shane Holloway ext:(%22) <shane.holloway@ieee.org>
2012-11-07 12:33:46 -0700

    Minor copyright adjustments. Added python test runner

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*- vim: set ts=4 sw=4 expandtab
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
-##~ Copyright (C) 2002-2012  TechGame Networks, LLC.              ##
+##~ Copyright (C) 2002-2012 Bellite.io                            ##
 ##~                                                               ##
 ##~ This library is free software; you can redistribute it        ##
 ##~ and/or modify it under the terms of the MIT style License as  ##
---
+++ test/testClientJSONRPC.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- vim: set ts=4 sw=4 expandtab
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+##~ Copyright (C) 2002-2012 Bellite.io                            ##
+##~                                                               ##
+##~ This library is free software; you can redistribute it        ##
+##~ and/or modify it under the terms of the MIT style License as  ##
+##~ found in the LICENSE file included with this distribution.    ##
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+
+import unittest, os, sys
+
+class TestJSONRPC(unittest.TestCase):
+    def testBelliteClient(self):
+        src = os.path.splitext(__file__)[0]+'.js'
+        assert 0==os.system('node '+src)
+
+
+if __name__=='__main__':
+    unittest.main()
+
